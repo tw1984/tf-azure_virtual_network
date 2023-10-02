d1044702aca5f0ffd0cad72b4364fadc5ef8e90b
tw1984 <woodlandt@gmail.com>
2023-10-01 19:06:38 -0700

    comment on address space

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = local.virtualNetworkName
   resource_group_name = var.resource_group_name
   location            = var.location
-  address_space       = var.address_space
+  address_space       = var.address_space //change to support multiple address spaces to align to Azure supported configurations
 
   lifecycle {
     ignore_changes = [tags]
